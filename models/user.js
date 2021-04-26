e1014e773ca55e24abcbb92e132c0e10f005a4f6
kanishk <kanishkupadhyay5@gmail.com>
2021-04-26 19:09:39 +0530

    add files

---
+++ models/user.js
@@ -0,0 +1,59 @@
+const mongoose=require("mongoose")
+const userSchema=new mongoose.Schema({
+    name:{
+        type:String,
+        required:true
+    },
+    email:{
+        type:String,
+        required:true
+    },
+    phone:{
+        type:Number,
+        required:true
+    },
+    password:{
+        type:String,
+        required:true
+    },
+    confirmPassword:{
+        type:String,
+        required:true
+    }
+})
+
+const moviesSchema=mongoose.Schema({
+    categoryId:{
+        type:Number,
+        required:true
+    },
+    name:{
+        type:String,
+        required:true
+    },
+    actor:{
+        type:String,
+        required:true
+    },
+    category:{
+        type:String,
+        required:true
+    },
+    iframe:{
+        type:String,
+        required:true
+    },
+    imgsrc:{
+        type:String,
+        required:true
+    },
+    uploadDate:{
+        type:String,
+        required:true
+    }
+})
+
+const User=mongoose.model('user',userSchema)
+
+const Movie=mongoose.model('movie',moviesSchema)
+module.exports={User,Movie}
